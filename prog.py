4ab238510bc5cddee3bbec2895c943d676e7ef44
bokov vlad <bokov.vlad@gmail.com>
2010-05-23 22:35:50 +0700

    new: file load, menu

--- prog.py
+++ prog.py
@@ -1,15 +1,17 @@
 #!/usr/bin/env python
 #-*- coding: utf-8 -*-
 
-import sys, re
+import sys,os, re
 from PyQt4 import QtCore, QtGui
 from gui import CrossTableView, LineEditDelegate
 from main import Crossword
+import pickle
 
 HEIGHT = 12
 WIDTH = 12
 COL_WIDTH = 35
 ROW_HIEGHT = 35
+CONFIG_DAT = 'config.dat'
 
 class ApplicationWindow(QtGui.QMainWindow):
     def __init__(self, parent = None):
@@ -18,10 +20,22 @@ class ApplicationWindow(QtGui.QMainWindow):
         self.setAttribute(QtCore.Qt.WA_DeleteOnClose)
         self.setWindowTitle("application main window")
 
+        ## menu ##
+        self.file_menu = QtGui.QMenu('&File', self)
+        self.file_menu.addAction('&Open', self.load_txt, QtCore.Qt.CTRL + QtCore.Qt.Key_H)
+        self.file_menu.addAction('&Quit', self.quit, QtCore.Qt.CTRL + QtCore.Qt.Key_Q)
+        self.menuBar().addMenu(self.file_menu)
+
+        self.help_menu = QtGui.QMenu('&Help', self)
+        self.menuBar().addSeparator()
+        self.menuBar().addMenu(self.help_menu)
+
+        self.help_menu.addAction('&About', self.about)
+        ## end menu ##
+
         self.main_widget = QtGui.QWidget(self)
-
         l = QtGui.QGridLayout(self.main_widget)
-
+        
 
         self.label = QtGui.QTextEdit()
         self.compute = QtGui.QPushButton('Compute')
@@ -35,7 +49,7 @@ class ApplicationWindow(QtGui.QMainWindow):
         #self._crossword = crossword
         
         self.table_view = CrossTableView()
-        self.compute_new()
+        self.load_prev_crossword()
 
         ## what's word under select coords ###
         self._table_rows = self.built_ass_table(False, HEIGHT)
@@ -62,13 +76,13 @@ class ApplicationWindow(QtGui.QMainWindow):
         #                # append it
         #                self.append_to_indexed_dic(n_col,{word.word:word.clue},self._table_cols)
         
-        #print 'self._table_rows.items'
-        #for k,v in self._table_rows.items():
-        #    print k,v     
+        print '\nself._table_rows.items'
+        for k,v in self._table_rows.items():
+            print k,v.keys()
             
-        #print 'self._table_cols.items'
-        #for k,v in self._table_cols.items():
-        #    print k,v     
+        print '\nself._table_cols.items'
+        for k,v in self._table_cols.items():
+            print k,v.keys()
             
 
         #model = QtGui.QStandardItemModel(WIDTH, HEIGHT)
@@ -92,19 +106,19 @@ class ApplicationWindow(QtGui.QMainWindow):
         self.statusBar().showMessage("Viva!", 2000)
 
         #self.connect(self.selection_model, QtCore.SIGNAL("selectionChanged ( QItemSelection, QItemSelection)"), self.selected_item_changed)
-        self.connect(self.compute, QtCore.SIGNAL("clicked()"), self.compute_new)
+        self.connect(self.compute, QtCore.SIGNAL("clicked()"), self.shuffle)
 
     def gen_html(self):
         html = '<html><body>'
         html += 'Horizontal<br>'
         hor = filter(lambda(x):not x.vertical,  self._crossword.current_word_list)
         for w in hor:
-            html += w.clue
+            html += w.clue.strip()
             html += '<br>'
         html += '<br>Vertical<br>'
         ver = filter(lambda(x):x.vertical,  self._crossword.current_word_list)
         for w in ver:
-            html += w.clue
+            html += w.clue.strip()
             html += '<br>'
         html += '</body></html>'
         return html 
@@ -132,10 +146,11 @@ class ApplicationWindow(QtGui.QMainWindow):
                         self.append_to_indexed_dic(n_row_col,{word.word:word.clue},table) 
         return table
 
+    def shuffle(self):
+        self._crossword.compute_crossword(2)
+        self.compute_new()
+
     def compute_new(self):
-        crossword = Crossword(WIDTH, HEIGHT, ' ', 5000, word_list)
-        crossword.compute_crossword(2)
-        self._crossword = crossword
     
         ## debug ####################################
         #import pickle, os 
@@ -150,10 +165,10 @@ class ApplicationWindow(QtGui.QMainWindow):
         #    crossword = pickle.load(open('dump_hor'))
         #self._crossword = crossword
 
-        print crossword.solution()
+        print self._crossword.solution()
         ################################################
 
-        #self.table_view = CrossTableView(self._crossword)
+        #self.table_view = CrossTableView(self._self._crossword)
         try:
             self.table_view.model().clear()
         except AttributeError:
@@ -166,7 +181,7 @@ class ApplicationWindow(QtGui.QMainWindow):
         self.selection_model = QtGui.QItemSelectionModel(model)
         self.table_view.setSelectionModel(self.selection_model)
 
-        delegate = LineEditDelegate(self.table_view, crossword)
+        delegate = LineEditDelegate(self.table_view, self._crossword)
         self.table_view.setItemDelegate(delegate)
 
         self.table_view.configure()
@@ -176,8 +191,33 @@ class ApplicationWindow(QtGui.QMainWindow):
         self._table_rows = self.built_ass_table(False, HEIGHT)
         self._table_cols = self.built_ass_table(True, WIDTH)
         self.label.setHtml(self.gen_html())
-        return crossword
+        #return self._crossword
 
+    def quit(self):
+        pickle.dump(self._crossword, open(CONFIG_DAT, 'w'))
+        self.close()
+
+    def load_prev_crossword(self):
+        if os.access(CONFIG_DAT, os.F_OK):
+            self._crossword = pickle.load(open(CONFIG_DAT))
+            self.compute_new()
+        else:
+            self.load_txt()
+
+    def about(self):
+        msg = QtGui.QMessageBox()
+        msg.setText("Author: Vlad Bokov")
+        msg.show()
+
+    def load_txt(self):
+        fileName = QtGui.QFileDialog.getOpenFileName(self, QtCore.QString("Open file"), os.getcwd(), QtCore.QString("Text files (*.txt)"))
+        raw = open(fileName).read()
+        words = re.findall('(\w+).*?=.*?(.*)', raw)
+        for i in words:
+            print i
+
+        crossword = Crossword(WIDTH, HEIGHT, ' ', 5000, words)
+        self._crossword = crossword
 
     def selected_item_changed(self, index, index2):
         curr_index = index.indexes()[0]
@@ -189,22 +229,20 @@ class ApplicationWindow(QtGui.QMainWindow):
             to_be_marked_by_col = self._table_cols[curr_index.column()]
         except KeyError:
             pass
-        #try:
-        to_be_marked = [to_be_marked_by_row[v] for v in  filter(lambda x: x in to_be_marked_by_col, to_be_marked_by_row)]
-        #except UnboundLocalError:
-        #    return
-        #print to_be_marked_by_row
-        MARK_TAG_BEFORE='<span style=" font-style:italic; color:#ff0000;">'
-        MARK_TAG_AFTER='</span>'
-        self.label.setHtml(self.label.toHtml().replace(MARK_TAG_BEFORE, ''))
-        self.label.setHtml(self.label.toHtml().replace(MARK_TAG_AFTER, ''))
 
+        to_be_marked = [to_be_marked_by_row[v] for v in  filter(lambda x: x in to_be_marked_by_col, to_be_marked_by_row)]
+        print to_be_marked
+        MARK_TAG_BEFORE='<i><font color="red">'
+        MARK_TAG_AFTER='</font></i>'
+
+        print '\nself.label.toPlainText('
         for m in to_be_marked:
-            self.label.setHtml(self.label.toHtml().replace(m, MARK_TAG_BEFORE+m+MARK_TAG_AFTER))
-        #print self.label.toHtml()
+            raw = m.strip()
+            t = self.label.toPlainText().replace(raw, MARK_TAG_BEFORE+raw+MARK_TAG_AFTER)
+            t = t.replace('\n', '<br>')
+            self.label.setHtml('<html><body>'+t+'</body></html>')
 
     def append_to_indexed_dic(self, index, value, dic):
-        #index += 1 # WHY?!
         if index not in dic:
             dic[index] = value
         elif index in dic: # and value not in dic[index]:
@@ -217,9 +255,6 @@ if __name__ == "__main__":
              ['leaven', 'An agent, such as yeast, that cause batter or dough to rise..'], \
              ['coda', 'Musical conclusion of a movement or composition.'], \
              ['paladin', 'A heroic champion or paragon of chivalry.']
-
-    #crossword = Crossword(WIDTH, HEIGHT, ' ', 5000, word_list)
-    #crossword.compute_crossword(2)
 
     app = QtGui.QApplication(sys.argv)
     wd = ApplicationWindow()
